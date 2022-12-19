89c73b01f224ec914c820cb32fc154c40215a53b
j8won <joowoni99@gmail.com>
2022-12-19 23:53:54 +0900

    feat(server): cors error handling

--- src/server/app.js
+++ src/server/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:3000'
+};
+
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.send('Welcome!');
