220bf97ecf3fb7efedf188fdddc42aa66cf7b37f
j8won <joowoni99@gmail.com>
2022-12-21 00:26:11 +0900

    chore(client): Initialize project environment

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  ThemeProvider as StyledThemeProvider,
+  createGlobalStyle,
+} from "styled-components";
+import reset from "styled-reset";
+import { HelmetProvider } from "react-helmet-async";
+
+import Theme from './styles/Theme';
+
+function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route path={'/'} element={<div>dd</div>} />
+      </Routes>
+    </Router>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HelmetProvider>
+      <StyledThemeProvider theme={Theme}>
+          <GlobalStyle />
+          <AppRouter />
+      </StyledThemeProvider>
+    </HelmetProvider>
   );
 }
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  i {
+    color: ${Theme.placeholderColor};
+  }
+
+  * {
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    text-decoration: none;
+    letter-spacing: -0.017em;
+  }
+
+  html {
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+
+  body {
+    font-family: 'Noto Sans KR', sans-serif;
+    font-weight: 400;
+  }
+
+  button {
+    padding: 0;
+  }
+`;
 
 export default App;
